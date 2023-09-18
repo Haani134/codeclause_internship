138319af08bd48b142cec72a4ad1d8e278cebe3a
Umme Hani <haniumme100@gmail.com>
2023-09-18 20:45:57 +0530

    udated the app.js

--- app.js
+++ app.js
@@ -2,6 +2,25 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 4000
-const server = app.listen(PORT,() => console.log('message server on port ${PORT}'))
+const server = app.listen(PORT,() => console.log(`message server on port ${PORT}`))
+const io = require('socket.io')(server)
 
 app.use(express.static(path.join(__dirname,'public')))
+
+let socketsConected = new Set()
+
+io.on('connection',onConnected)
+    
+function onConnected(socket) {
+    console.log(socket.id)
+    socketsConected.add(socket.id)
+
+
+    io.emit('clients-total',socketsConected.size)
+    socket.on('disconnect', () => {
+        console.log('Socket disconnected',socket.id)
+        socketsConected.delete(socket.id)
+        io.emit('clients-total',socketsConected.size)
+    })
+
+}
