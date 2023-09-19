f8f2c16b538384804745d7dcf7b89f971048e6d9
Umme Hani <haniumme100@gmail.com>
2023-09-19 16:51:24 +0530

    added the required udates

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 4000
 const server = app.listen(PORT,() => console.log(`message server on port ${PORT}`))
+
 const io = require('socket.io')(server)
 
 app.use(express.static(path.join(__dirname,'public')))
@@ -12,19 +13,20 @@ let socketsConected = new Set()
 io.on('connection',onConnected)
     
 function onConnected(socket) {
-    console.log(socket.id)
+    console.log('Socket connected', socket.id)
     socketsConected.add(socket.id)
-
-
     io.emit('clients-total',socketsConected.size)
+
     socket.on('disconnect', () => {
         console.log('Socket disconnected',socket.id)
         socketsConected.delete(socket.id)
-
         io.emit('clients-total',socketsConected.size)
     })
        socket.on('message', (data) => {
-        console.log(data)
         socket.broadcast.emit('chat-message',data)
+       })
+
+       socket.on('feedback', (data) => {
+        socket.broadcast.emit('feedback', data)
        })
 }
